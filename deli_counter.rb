03c2a9e2718b02497a6c365565d351f32c84026f
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-01 22:13:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ require "pry"
 def line(array) 
   if array.size == 0 
     return "The line is currently empty."
-  else
-    array.each_with_index do |name, number|
+  #else
+    #array.each_with_index do |name, number|
   
   end
 end
