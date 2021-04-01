664c1f11c975f2ebd5ae8a9103f828e01bf62633
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-01 22:48:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,11 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{position} in line."
 end 
 
-def now_serving
-  
+def now_serving(array)
+  if array.size > 0 
+    puts "Currently serving #{array[0]}."
+    array.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end 
 end
