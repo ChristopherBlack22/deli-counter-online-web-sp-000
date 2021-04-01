dd7a6606594a997d0a783d62c75f0586f8a1e11c
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-01 22:36:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,23 @@
 require "pry"
 
 def line(array) 
-  message = "The line is currently:"
+  default_message = "The line is currently:"
   if array.size == 0 
-    line_status = message.delete(":")
-    puts line_status << " empty."
+    no_people = default_message.delete(":")
+    puts no_people << " empty."
   else
     array.each_with_index do |name, number|
       position = number + 1
-      message << " #{position}. #{name}"
+      default_message << " #{position}. #{name}"
     end
-    puts message
+    puts default_message
   end 
 end
 
-def take_a_number
-  
+def take_a_number(array, name)
+  array << name
+  position = array.size + 1
+  "Welcome, #{name}. You are number #{position} in line."
 end 
 
 def now_serving
